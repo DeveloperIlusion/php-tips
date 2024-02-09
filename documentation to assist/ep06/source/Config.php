707e730663a79b4776a39bf6a0ab419cccc0563e
Bruno Cassiano <119907931+DeveloperIlusion@users.noreply.github.com>
2024-02-09 16:16:35 -0300

    repositório configurado

---
+++ documentation to assist/ep06/source/Config.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+
+define("FACEBOOK", [
+    "app_id" => "",
+    "app_secret" => "",
+    "app_redirect" => "https://localhost/cursos/ep06",
+    "app_version" => "v4.0",
+]);
